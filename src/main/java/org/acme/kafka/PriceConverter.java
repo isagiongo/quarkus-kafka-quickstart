0ed7641b187cd436e4b8b2691b7948ad84d3a86b
Isadora Giongo <isagiongo@gmail.com>
2020-04-26 11:57:50 -0300

    First commit, running project

---
+++ src/main/java/org/acme/kafka/PriceConverter.java
@@ -0,0 +1,20 @@
+package org.acme.kafka;
+
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PriceConverter {
+
+    public static final double CONVERSION_RATE = 0.88;
+
+    @Incoming("prices")
+    @Outgoing("my-data-stream")
+    @Broadcast
+    public double process(int priceInUsd) {
+        return priceInUsd * CONVERSION_RATE;
+    }
+}
